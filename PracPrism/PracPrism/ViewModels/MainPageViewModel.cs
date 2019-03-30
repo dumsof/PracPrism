70b66c221aaf1243d516e596a21cade88d265c98
Darwin <darwinu34@yahoo.es>
2019-03-30 15:31:35 -0500

    practica 1 se inicia el direciconamiento a una nueva pagina.

---
+++ PracPrism/PracPrism/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PracPrism.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private readonly IPageDialogService dialogService;
+        private readonly INavigationService navigationService;
+        public DelegateCommand PracticaBotonClick { get; }
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            this.navigationService = navigationService;
+            this.dialogService = dialogService;
+            PracticaBotonClick = new DelegateCommand(async () => await PracticaDelegateCommandAsync());
+        }
+
+        private async Task PracticaDelegateCommandAsync()
+        {
+            //se agrega el servicio de dialogo service, esto con el fin de mostrar mensaje de alerta.
+            bool answer = await this.dialogService.DisplayAlertAsync("Pregunta?", "Deseas dirigirte a pagina de practica.", "Yes", "No");            
+            Debug.WriteLine("Respuesta: " + answer);
+            await this.navigationService.NavigateAsync("practica");
+        }
+    }
+}
---
+++ PracPrism/PracPrism/ViewModels/PaginaPracticaViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace PracPrism.ViewModels
+{
+    using Prism.Commands;
+    using Prism.Navigation;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class PaginaPracticaViewModel : ViewModelBase
+    {
+        private INavigationService navigationService;       
+
+        /// <summary>
+        /// DUM: se debe crear el constructor y heredar de la clase base y pasar la navegación.
+        /// </summary>
+        /// <param name="navigationService"></param>
+        public PaginaPracticaViewModel(INavigationService navigationService):base(navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+    }
+}
